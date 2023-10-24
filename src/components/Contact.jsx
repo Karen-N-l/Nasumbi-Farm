1ab1f481a7d51feaa6bf867dcd0fc6d51b311dd9
Karen-N-l <karenloti45@gmail.com>
2023-10-24 10:59:14 +0300

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
     <div className="w-full h-screen bg-white flex justify-center items-center p-4">
       
         <form onSubmit={handleFormSubmit} className="flex flex-col max-w-[600px] w-full  ">
-          <div className="pb-0 pt-32">
+          <div className="pb-0 pt-20">
             <p className="text-[#076C05] text-3xl font-bold">Contact Us</p>
           </div>
           <input
