73d3e0ff3375cbd1d16f596398b2e4aeac4e11af
Karen-N-l <karenloti45@gmail.com>
2023-10-24 08:26:38 +0300

    lazy loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Background1 from '../assets/Cereals.jpg';
-import Background2 from '../assets/vineyard.jpg';
+import Background1 from '../assets/vineyard.jpg';
+import Background2 from '../assets/Cereals.jpg';
 import Background3 from '../assets/paprika.jpg'; // Add Background3
 import { Link } from 'react-router-dom';
-
-
 
 const Home = () => {
   const backgrounds = [Background1, Background2, Background3]; // Add Background3 to the array
@@ -20,29 +18,26 @@ const Home = () => {
     };
   }, [backgrounds.length]);
 
+  const backgroundStyle = {
+    backgroundImage: `url(${backgrounds[currentBackgroundIndex]})`,
+  };
+
   return (
     <div>
-      
-    <div  className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgrounds[currentBackgroundIndex]})` }}>
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <h2 className="text-4xl font-bold text-[#A8D34B] mb-4">Welcome to Nasumbi </h2>
-        <p className="text-white text-xl">Discover our diverse crops and thriving poultry and livestock.</p>
-        <div >
-        <Link to='/about'smooth={true} duration={500}>
-          <button className="bg-[#A8D34B]  border-2 border-[#A8D34B] px-4 py-3 my-8 rounded-md  text-white">
-          Discover More
-          </button>
-          </Link>
+      <div className="bg-cover bg-center h-screen" style={backgroundStyle}>
+        <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+          <h2 className="text-4xl font-bold text-[#A8D34B] mb-4">Welcome to Nasumbi </h2>
+          <p className="text-white text-xl">Discover our diverse crops and thriving poultry and livestock.</p>
+          <div>
+            <Link to="/about" smooth={true} duration={500}>
+              <button className="bg-[#A8D34B] border-2 border-[#A8D34B] px-4 py-3 my-8 rounded-md text-white">
+                Discover More
+              </button>
+            </Link>
+          </div>
         </div>
-        
-
       </div>
-      
     </div>
-    
-
-    </div>
-    
   );
 }
 
